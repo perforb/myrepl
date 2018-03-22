64aafceb572fae1069d84c94eee00cf8e7e56b4e
perforb <dev.perfumed.garden@gmail.com>
2018-03-22 20:48:24 +0900

    Added arguments parser

--- myreplica/excecutor.py
+++ myreplica/excecutor.py
@@ -1,8 +1,54 @@
 # -*- coding: utf-8 -*-
 
+import sys
+import argparse
+import logging
+
+logger = logging.getLogger('myreplica')
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
+
+
+def parse_argument():
+    parser = argparse.ArgumentParser()
+    parser.set_defaults(
+        source=None,
+        destination=None,
+        dbsettings='/etc/myreplica.ini',
+        dryrun=True,
+        verbose=False,
+    )
+
+    parser.add_argument('-s',
+                        '--source',
+                        required=True,
+                        dest='source_env',
+                        help='source environment')
+    parser.add_argument('-d',
+                        '--destination',
+                        required=True,
+                        dest='dest_env',
+                        help='destination environment')
+    parser.add_argument('--dryrun',
+                        action='store_true',
+                        help='dry run if this option is passed.')
+    parser.add_argument('-v',
+                        '--verbose',
+                        action='store_true',
+                        help='set verbose mode')
+
+    args = parser.parse_args()
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+
+    return args
+
 
 def main():
-    pass
+    args = parse_argument()
+    logger.debug(args)
+
 
 if __name__ == '__main__':
     main()
